2b9e60c7a9d3f9125480580f95b1af20994d0336
Sanjay Behera <sbehera@Sbeheras-Mac.local>
2016-09-22 00:31:25 +0530

    refactored

---
+++ client/glass.js
@@ -0,0 +1,181 @@
+var container;
+var camera, scene, renderer, controls;
+var mouseX = 0, mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+init();
+animate();
+
+var abc = 1 ;
+
+function init() {
+	container = document.getElementById('3dContainer');
+
+ 	camera = new THREE.OrthographicCamera(
+    (window.innerWidth / -2),   // Left
+    (window.innerWidth / 2),    // Right
+    (window.innerHeight / 2),   // Top
+    (window.innerHeight / -2),  // Bottom
+    -10000,            // Near clipping plane
+    10000 );           // Far clipping plane
+
+
+ 	camera.position.y = 43;
+
+ 	camera.position.x= 6;
+ 	camera.position.z= -194;
+
+ 	camera.zoom = 0.120;
+
+	camera.updateProjectionMatrix();
+
+	// scene
+	scene = new THREE.Scene();
+	var ambient = new THREE.AmbientLight( 0x101030 );
+	scene.add( ambient );
+	var directionalLight = new THREE.DirectionalLight( 0xE9BDA8 );
+	directionalLight.position.set( 1, 1, 1 );
+	scene.add( directionalLight );
+
+
+	var extraLight = new THREE.DirectionalLight( 0xffffff );
+	extraLight.position.set( -1, -1, -1 );
+	scene.add( extraLight );
+
+	scene.add(camera);
+
+
+	// texture
+	var manager = new THREE.LoadingManager();
+	manager.onProgress = function ( item, loaded, total ) {
+		console.log( item, loaded, total );
+	};
+	var texture = new THREE.Texture();
+	var onProgress = function ( xhr ) {
+		if ( xhr.lengthComputable ) {
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( Math.round(percentComplete, 2) + '% downloaded' );
+		}
+	};
+	var onError = function ( xhr ) {
+	};
+
+	var loader = new THREE.ImageLoader( manager );
+	loader.load( 'assets/glass-texture.jpg', function ( image ) {
+		texture.image = image;
+		texture.needsUpdate = true;
+	} );
+
+
+	var loader = new THREE.OBJLoader( manager );
+	loader.load( 'assets/glass.obj', function ( object ) {
+		object.traverse( function ( child ) {
+			if ( child instanceof THREE.Mesh ) {
+				child.material.map = texture;
+				child.scale.set(15,15,15);
+				// child.scale.set(100,100,100);
+			}
+		} );
+		object.position.y = -95;
+
+	    // object.position.x = 0;
+	    // object.position.y = 0;
+	    // object.position.z = 0;
+
+		scene.add( object );
+	}, onProgress, onError );
+
+
+	renderer = new THREE.WebGLRenderer({ alpha: true });
+	renderer.setPixelRatio( window.devicePixelRatio );
+	// renderer.setClearColor( 0xffffff, 0 );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+
+	container.appendChild( renderer.domElement );
+
+
+
+	controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.enableZoom = true;
+
+	window.addEventListener('resize', onWindowResize, false);
+
+}
+function onWindowResize() {
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+var tempX =0, tempY =0, tempZ =0;
+
+ function animate() {
+
+ 	var x = Math.floor(camera.position.x);
+	var y = Math.floor(camera.position.y);
+	var z = Math.floor(camera.position.z);
+
+	if(tempX != x || tempY != y || tempZ != z){
+		console.log("camera.position.x="+camera.position.x+"; camera.position.y="+camera.position.y+"; camera.position.z="+camera.position.z+"; camera.zoom="+camera.zoom);
+		tempX = x ;
+		tempY = y ;
+		tempZ = z ;
+	}
+
+	if(isMovingRight){
+		moveCameraRight();
+		isMovingRight = false ;
+	}else if(isMovingLeft){
+		moveCameraLeft();
+		isMovingLeft = false ;
+	}
+
+	if(isMovingUp){
+		if(camera.position.y < 160){
+			camera.position.y += 20;
+		}
+		isMovingUp = false ;
+	}else if(isMovingDown){
+		if(camera.position.y > 20){
+			camera.position.y -= 20;
+		}
+		
+		isMovingDown = false ;
+	}
+	
+
+    requestAnimationFrame(animate);
+
+    controls.update();
+
+
+
+    render();
+
+}
+
+function moveCameraRight(){
+	camera.position.x = Math.floor(Math.cos( abc ) * 200);
+	camera.position.z = Math.floor(Math.sin( abc ) * 200);
+
+	abc += 100 ;
+}
+
+function moveCameraLeft(){
+	camera.position.x = Math.floor(Math.sin( abc ) * 200);
+	camera.position.z = Math.floor(Math.cos( abc ) * 200);
+
+	abc += 100;
+}
+function render() {
+	
+  camera.lookAt( scene.position ); 
+  renderer.render(scene, camera);
+
+}
+
--- server/server.js
+++ server/server.js
@@ -29,24 +29,15 @@ var handleRequest = function(request, response) {
     } else if(request.url == '/style.css') {
         response.writeHead(200, {'Content-Type': 'text/css'});
         response.end(fs.readFileSync('client/style.css'));
-    } else if(request.url == '/3dmodel.js') {
+    } else if(request.url == '/car.js') {
         response.writeHead(200, {'Content-Type': 'application/javascript'});
-        response.end(fs.readFileSync('client/3dmodel.js'));
-    }else if(request.url == '/3dmodel2.js') {
+        response.end(fs.readFileSync('client/car.js'));
+    }else if(request.url == '/heart.js') {
         response.writeHead(200, {'Content-Type': 'application/javascript'});
-        response.end(fs.readFileSync('client/3dmodel2.js'));
-    }else if(request.url == '/3dmodel3.js') {
+        response.end(fs.readFileSync('client/heart.js'));
+    }else if(request.url == '/glass.js') {
         response.writeHead(200, {'Content-Type': 'application/javascript'});
-        response.end(fs.readFileSync('client/3dmodel3.js'));
-    }else if(request.url == '/3dmodel4.js') {
-        response.writeHead(200, {'Content-Type': 'application/javascript'});
-        response.end(fs.readFileSync('client/3dmodel4.js'));
-    }else if(request.url == '/3dmodel5.js') {
-        response.writeHead(200, {'Content-Type': 'application/javascript'});
-        response.end(fs.readFileSync('client/3dmodel5.js'));
-    }else if(request.url == '/3dmodel6.js') {
-        response.writeHead(200, {'Content-Type': 'application/javascript'});
-        response.end(fs.readFileSync('client/3dmodel6.js'));
+        response.end(fs.readFileSync('client/glass.js'));
     } else if(request.url == '/three.js') {
         response.writeHead(200, {'Content-Type': 'application/javascript'});
         response.end(fs.readFileSync('client/three.js'));
@@ -74,42 +65,18 @@ var handleRequest = function(request, response) {
     } else if(request.url == '/jquery-3.1.0.js') {
         response.writeHead(200, {'Content-Type': 'application/javascript'});
         response.end(fs.readFileSync('client/jquery-3.1.0.js'));
-    } else if(request.url == '/assets/female-croupier-2013-03-26.mtl') {
-        response.end(fs.readFileSync('client/assets/female-croupier-2013-03-26.mtl'));
-    } else if(request.url == '/assets/female-croupier-2013-03-26.obj') {
-        response.end(fs.readFileSync('client/assets/female-croupier-2013-03-26.obj'));
-    } else if(request.url == '/assets/female-croupier-2013-03-26.png') {
-        response.end(fs.readFileSync('client/assets/female-croupier-2013-03-26.png'));
-    } else if(request.url == '/assets/UV_Grid_Sm.jpg') {
-        response.end(fs.readFileSync('client/assets/UV_Grid_Sm.jpg'));
-    } else if(request.url == '/assets/heart-texture-1.jpg') {
-        response.end(fs.readFileSync('client/assets/heart-texture-1.jpg'));
-    } else if(request.url == '/assets/heart-texture-2.jpg') {
-        response.end(fs.readFileSync('client/assets/heart-texture-2.jpg'));
-    } else if(request.url == '/assets/heart-texture-3.jpg') {
-        response.end(fs.readFileSync('client/assets/heart-texture-3.jpg'));
-    } else if(request.url == '/assets/dna_texture_1.jpg') {
-        response.end(fs.readFileSync('client/assets/dna_texture_1.jpg'));
-    } else if(request.url == '/assets/male02.obj') {
-        response.end(fs.readFileSync('client/assets/male02.obj'));
-    } else if(request.url == '/assets/heart.obj') {
+    } else if(request.url == '/assets/heart-texture.jpg') {
+        response.end(fs.readFileSync('client/assets/heart-texture.jpg'));
+    }else if(request.url == '/assets/heart.obj') {
         response.end(fs.readFileSync('client/assets/heart.obj'));
-    } else if(request.url == '/assets/dna.obj') {
-        response.end(fs.readFileSync('client/assets/dna.obj'));
-    } else if(request.url == '/assets/car1.obj') {
-        response.end(fs.readFileSync('client/assets/car1.obj'));
-    } else if(request.url == '/assets/car1.mtl') {
-        response.end(fs.readFileSync('client/assets/car1.mtl'));
-    } else if(request.url == '/assets/car2.obj') {
+    }else if(request.url == '/assets/car2.obj') {
         response.end(fs.readFileSync('client/assets/car2.obj'));
     } else if(request.url == '/assets/car2.mtl') {
         response.end(fs.readFileSync('client/assets/car2.mtl'));
-    } else if(request.url == '/assets/car4.obj') {
-        response.end(fs.readFileSync('client/assets/car4.obj'));
-    } else if(request.url == '/assets/car4.mtl') {
-        response.end(fs.readFileSync('client/assets/car4.mtl'));
-    } else if(request.url == '/assets/car3.obj') {
-        response.end(fs.readFileSync('client/assets/car3.obj'));
+    }else if(request.url == '/assets/glass.obj') {
+        response.end(fs.readFileSync('client/assets/glass.obj'));
+    } else if(request.url == '/assets/glass-texture.jpg') {
+        response.end(fs.readFileSync('client/assets/glass-texture.jpg'));
     }
 
 
